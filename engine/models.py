b31f07ab49361553cd41b462fee4f6bea369aa2d
Waldemar Sojka <waldemar.sojka@gmail.com>
2012-12-18 13:36:42 +0100

    Update engine/models.py
    Added model representation for expense buckets.

--- engine/models.py
+++ engine/models.py
@@ -5,6 +5,14 @@ class Currency(models.Model):
     def __unicode__(self):
         return self.code
 
+class Store(models.Model):
+    name = models.CharField(max_length = 100)
+
+class FormOfPayment(models.Model):
+    name = models.CharField(max_length = 100)
+
+class ExpenseType(models.Model):
+    name = models.CharField(max_length = 100)
 
 class User(models.Model):
     username = models.CharField(max_length = 100)
@@ -13,8 +21,16 @@ class User(models.Model):
         return self.username
 
 class Expense(models.Model):
-    transaction_id = models.IntegerField()
-    date = models.DateField()
-    name = models.CharField(max_length = 100)
+    #transaction_id = models.IntegerField()
+    #date = models.DateField()
+    #name = models.CharField(max_length = 100)
+    expense_type = models.ForeighKey(ExpenseType)
     amount = models.DecimalField(max_digits = 10, decimal_places = 5)
     currency = models.ForeignKey(Currency)
+    comment = models.CharField(max_length = 100)
+
+class ExpenseBucket(models.Model):
+    date = models.DateField()
+    store = models.ForeignKey(Store)
+    form_of_payment = models.ForeignKey(FirmOfPayment)
+    
